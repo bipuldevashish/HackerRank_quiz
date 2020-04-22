f6df883b2653d1dd8c75d226012f0a9f178d8276
Bipul Devashish <bipuldevashish@gmail.com>
2020-04-22 12:14:15 +0530

    Add files via upload

---
+++ concate.py
@@ -0,0 +1,29 @@
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = 4
+b = 4.0
+c = 'is the best coding platform'
+# Read and save an integer, double, and String to your variables.
+a = input("enter an integer")
+b = input("enter a double")
+c = input("enter a string")
+# Print the sum of both integer variables on a new line.
+#NOTE : YOU NEED TO CONVERT STRING INTO INTEGER AS PYTHON TAKES INTEGER AS DEFAULT
+a = int(a)
+a = i + a
+print(a)
+
+# Print the sum of the double variables on a new line.
+b = float(b)
+b = b + d
+print(b)
+
+
+# Concatenate and print the String variables on a new line
+c = s + c
+print(c)
+
+# The 's' variable above should be printed first.
